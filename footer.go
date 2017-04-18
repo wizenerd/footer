7316ca153563487d3a3bb96261f1f0cdfbe54751
gernest <geofreyernest@live.com>
2017-04-18 13:48:55 +0300

    finalize initial structures

--- footer.go
+++ footer.go
@@ -37,20 +37,33 @@ func (f *Footer) Render() *vecty.HTML {
 }
 
 type Section struct {
+	vecty.Core
 	Kind     Kind
+	Top      bool
 	Left     bool
 	Right    bool
+	Bottom   bool
+	Middle   bool
 	Children vecty.MarkupOrComponentOrHTML
 }
 
 func (s *Section) Render() *vecty.HTML {
 	c := make(vecty.ClassMap)
 	pos := ""
+	if s.Top {
+		pos = "top"
+	}
 	if s.Left {
 		pos = "left"
 	}
 	if s.Right {
 		pos = "right"
+	}
+	if s.Bottom {
+		pos = "bottom"
+	}
+	if s.Middle {
+		pos = "middle"
 	}
 	c["mdl-"+s.Kind.String()+"-footer__"+pos+"-section"] = true
 	return elem.Div(
@@ -59,6 +72,7 @@ func (s *Section) Render() *vecty.HTML {
 }
 
 type Logo struct {
+	vecty.Core
 	Children vecty.MarkupOrComponentOrHTML
 }
 
@@ -71,6 +85,7 @@ func (l *Logo) Render() *vecty.HTML {
 }
 
 type SociatButton struct {
+	vecty.Core
 	Kind     Kind
 	Children vecty.MarkupOrComponentOrHTML
 }
@@ -78,12 +93,27 @@ type SociatButton struct {
 func (s *SociatButton) Render() *vecty.HTML {
 	c := make(vecty.ClassMap)
 	c["mdl-"+s.Kind.String()+"-footer__social-btn"] = true
+	return elem.Button(
+		c, s.Children,
+	)
+}
+
+type DropDown struct {
+	vecty.Core
+	Kind     Kind
+	Children vecty.MarkupOrComponentOrHTML
+}
+
+func (s *DropDown) Render() *vecty.HTML {
+	c := make(vecty.ClassMap)
+	c["mdl-"+s.Kind.String()+"-footer__drop-down-section"] = true
 	return elem.Div(
 		c, s.Children,
 	)
 }
 
 type LinkList struct {
+	vecty.Core
 	Kind     Kind
 	Children vecty.MarkupOrComponentOrHTML
 }
@@ -91,7 +121,16 @@ type LinkList struct {
 func (l *LinkList) Render() *vecty.HTML {
 	c := make(vecty.ClassMap)
 	c["mdl-"+l.Kind.String()+"-footer__link-list"] = true
-	return elem.Div(
+	return elem.UnorderedList(
 		c, l.Children,
 	)
+}
+
+func H1(k Kind, m ...vecty.MarkupOrComponentOrHTML) *vecty.HTML {
+	c := make(vecty.ClassMap)
+	c["mdl-"+k.String()+"-footer__heading"] = true
+	var v vecty.List
+	v = append(v, c)
+	v = append(v, m...)
+	return elem.Heading1(v)
 }
